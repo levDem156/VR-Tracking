66abac079f1ad52dd742dc5700b12e00e666fe09
Lev <110845634+levDem156@users.noreply.github.com>
2025-05-11 15:56:50 +0500

    Update script.js

--- vr_send/script.js
+++ vr_send/script.js
@@ -49,7 +49,7 @@ window.addEventListener('resize', () => {
 });
 
 // Сервер и элементы управления
-const server_url = "https://olive-eyes-sip.loca.lt";
+const server_url = "https://cyan-states-say.loca.lt";
 const output = document.getElementById('output');
 const startButton = document.getElementById('startButton');
 
