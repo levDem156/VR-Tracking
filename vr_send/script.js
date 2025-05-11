c9865419d2af3d8b7430fcaf76f4c009928c8cfe
Lev <110845634+levDem156@users.noreply.github.com>
2025-05-11 21:11:43 +0500

    Update script.js

--- vr_send/script.js
+++ vr_send/script.js
@@ -49,7 +49,7 @@ window.addEventListener('resize', () => {
 });
 
 // Сервер и элементы управления
-const server_url = "https://cyan-states-say.loca.lt";
+const server_url = "http://rnzbh-91-192-170-127.a.free.pinggy.link";
 const output = document.getElementById('output');
 const startButton = document.getElementById('startButton');
 
