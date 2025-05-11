3974f1117ebd3d0aa3cb77d1985955092e979b2b
Lev <110845634+levDem156@users.noreply.github.com>
2025-05-11 08:26:09 +0500

    Update script.js

--- vr_send/script.js
+++ vr_send/script.js
@@ -49,7 +49,7 @@ window.addEventListener('resize', () => {
 });
 
 // Сервер и элементы управления
-const server_url = "https://cg-imaging-hand-tb.trycloudflare.com";
+const server_url = "https://distribute-claire-hartford-happening.trycloudflare.com";
 const output = document.getElementById('output');
 const startButton = document.getElementById('startButton');
 
